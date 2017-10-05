5260a93c5c6f42f86df4936a1f649f60fafd2838
Babak <baamira@yahoo.com>
2017-10-05 11:24:26 -0700

    going back to using daysPay *= 2 instead of pow

--- CIS55Lab1B.swift
+++ CIS55Lab1B.swift
@@ -1,13 +1,13 @@
 import UIKit
 let numberOfDays: Int = 32
 var dailyPayArray: [Int] = []; var totalPayArray: [Int] = []
-var totalPay: Int =  0;
+var daysPay: Int = 1;  var totalPay: Int =  0;
 for n in (0...numberOfDays-1){
-    let daysPay: Int = Int(truncating: NSDecimalNumber(decimal: pow(2, n)))
     totalPay += daysPay
     dailyPayArray.append(daysPay); totalPayArray.append(totalPay);
-    let formatter = NumberFormatter()
+    let formatter = NumberFormatter() // for dollar amounts
     formatter.numberStyle = .currency; formatter.formatWidth = 20
     let outputFormat = NSString(format:"| day %-3d | %@ | %@ |", n + 1, formatter.string(from: Double(dailyPayArray[n])/100.0 as NSNumber)!, formatter.string(from: Double(totalPayArray[n])/100.0 as NSNumber)!)
     print("\(outputFormat)")
+    daysPay *= 2
 }
