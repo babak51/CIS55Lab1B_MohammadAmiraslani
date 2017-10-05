f07efef3a7da3a3313467aba16cbb14c696b47c7
Babak <baamira@yahoo.com>
2017-10-05 10:51:09 -0700

    changing var names

--- CIS55Lab1B.swift
+++ CIS55Lab1B.swift
@@ -1,11 +1,11 @@
 import UIKit
 let numberOfDays: Int = 32
 var dailyPayArray: [Int] = []; var totalPayArray: [Int] = []
-var total: Int =  0;
+var totalPay: Int =  0;
 for n in (0...numberOfDays-1){
-    let nextDaysPay: Int = Int(truncating: NSDecimalNumber(decimal: pow(2, n)))
-    total += nextDaysPay
-    dailyPayArray.append(nextDaysPay); totalPayArray.append(total);
+    let daysPay: Int = Int(truncating: NSDecimalNumber(decimal: pow(2, n)))
+    totalPay += daysPay
+    dailyPayArray.append(daysPay); totalPayArray.append(totalPay);
     let formatter = NumberFormatter()
     formatter.numberStyle = .currency; formatter.formatWidth = 20
     let outputFormat = NSString(format:"| day %-3d | %@ | %@ |", n + 1, formatter.string(from: Double(dailyPayArray[n])/100.0 as NSNumber)!, formatter.string(from: Double(totalPayArray[n])/100.0 as NSNumber)!)
