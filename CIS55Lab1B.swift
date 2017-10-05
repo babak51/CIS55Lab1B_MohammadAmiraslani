bea772990749ae3954876fc2e29dd750011e9bc3
Babak <baamira@yahoo.com>
2017-10-05 03:06:08 -0700

    user NumberFormatter for currency

--- CIS55Lab1B.swift
+++ CIS55Lab1B.swift
@@ -1,13 +1,13 @@
 import UIKit
-let numberOfDays: Int = 32     // number of days
-var dailyPayArray: [Int] = []  // empty array which would keep daily pay
-var totalPayArray: [Int] = []  // empty array which would keep total pay
+let numberOfDays: Int = 32
+var dailyPayArray: [Int] = []; var totalPayArray: [Int] = []
 var total: Int =  0; var nextDaysPay: Int = 1
 for n in (1...numberOfDays){
-    dailyPayArray.append(nextDaysPay)
     total += nextDaysPay
-    totalPayArray.append(total)
-    let outputFormat = NSString(format:"| day %3d | %12.2f | %12.2f |", n, Double(dailyPayArray[n-1])/100.0, Double(totalPayArray[n-1])/100.0)
+    dailyPayArray.append(nextDaysPay); totalPayArray.append(total);
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .currency; formatter.formatWidth = 20
+    let outputFormat = NSString(format:"| day %-3d | %@ | %@ |", n, formatter.string(from: Double(dailyPayArray[n-1])/100.0 as NSNumber)!, formatter.string(from: Double(totalPayArray[n-1])/100.0 as NSNumber)!)
     print("\(outputFormat)")
     nextDaysPay *= 2
 }
