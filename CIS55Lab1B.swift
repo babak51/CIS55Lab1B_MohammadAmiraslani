7c4afeab75223edfbcf418871b81e3246d6ac2f4
Babak <baamira@yahoo.com>
2017-10-05 03:22:33 -0700

    changing the range in the for-loop

--- CIS55Lab1B.swift
+++ CIS55Lab1B.swift
@@ -2,12 +2,12 @@ import UIKit
 let numberOfDays: Int = 32
 var dailyPayArray: [Int] = []; var totalPayArray: [Int] = []
 var total: Int =  0; var nextDaysPay: Int = 1
-for n in (1...numberOfDays){
+for n in (0...numberOfDays-1){
     total += nextDaysPay
     dailyPayArray.append(nextDaysPay); totalPayArray.append(total);
     let formatter = NumberFormatter()
     formatter.numberStyle = .currency; formatter.formatWidth = 20
-    let outputFormat = NSString(format:"| day %-3d | %@ | %@ |", n, formatter.string(from: Double(dailyPayArray[n-1])/100.0 as NSNumber)!, formatter.string(from: Double(totalPayArray[n-1])/100.0 as NSNumber)!)
+    let outputFormat = NSString(format:"| day %-3d | %@ | %@ |", n, formatter.string(from: Double(dailyPayArray[n])/100.0 as NSNumber)!, formatter.string(from: Double(totalPayArray[n])/100.0 as NSNumber)!)
     print("\(outputFormat)")
     nextDaysPay *= 2
 }
