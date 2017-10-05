121a6c71c831cf11e88e82ac9e088eff299bc3e9
Babak <baamira@yahoo.com>
2017-10-05 11:40:20 -0700

    taking out a ;

--- CIS55Lab1B.swift
+++ CIS55Lab1B.swift
@@ -4,7 +4,7 @@ var dailyPayArray: [Int] = []; var totalPayArray: [Int] = []
 var daysPay: Int = 1;  var totalPay: Int =  0;
 for n in (0...numberOfDays-1){
     totalPay += daysPay
-    dailyPayArray.append(daysPay); totalPayArray.append(totalPay);
+    dailyPayArray.append(daysPay); totalPayArray.append(totalPay)
     let formatter = NumberFormatter() // for dollar amounts
     formatter.numberStyle = .currency; formatter.formatWidth = 20
     let outputFormat = NSString(format:"| day %-3d | %@ | %@ |", n + 1, formatter.string(from: Double(dailyPayArray[n])/100.0 as NSNumber)!, formatter.string(from: Double(totalPayArray[n])/100.0 as NSNumber)!)
